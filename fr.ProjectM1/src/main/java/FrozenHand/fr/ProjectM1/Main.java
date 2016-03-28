40fc8e3c2d8f26b208e30a39de295c7ffa984347
Irodium <jeff_perio@hotmail.fr>
2016-03-28 11:31:16 +0200

    Structure #1

---
+++ fr.ProjectM1/src/main/java/FrozenHand/fr/ProjectM1/Main.java
@@ -0,0 +1,52 @@
+
+public class Main {
+
+	
+	
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        MainJFrame frame = new MainJFrame();
+        
+        
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+            	frame.setVisible(true);
+            }
+        });
+        
+    	TaskModel taskModel = new TaskModel(frame.getFile());
+		 StateChart stateChart = new StateChart();
+		
+		
+		
+		 Translate trans = new Translate(taskModel, stateChart);
+        
+    }
+	
+
+	
+	
+}
---
+++ fr.ProjectM1/src/main/java/FrozenHand/fr/ProjectM1/TaskModel.java
@@ -0,0 +1,17 @@
+import java.io.File;
+
+public class TaskModel 
+{
+	private File xmlFile;
+	
+	public TaskModel(File file)
+	{
+		xmlFile = file;
+	}
+	
+	public TaskModel()
+	{
+		
+	}
+
+}
